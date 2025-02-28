2f76604f92fcf38b15fc80a6734f582de907a8ef
Daniel Molnar <daniel.molnar@blackmirror.media>
2025-02-28 15:36:20 +0100

    Adding installation script

---
+++ src/install.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once dirname(__FILE__) . '/library/bootstrap.php';
+$app = new bootstrap();
+
+$rootPath = '/var/www/html/';
+$newApiFile = dirname(__FILE__) . '/install/api.php';
+$cfgPattern = "/'advert\-sites'\s*=>\s*\[[\s\S]*?\]/s";
+$cfgReplacement = "'advert-sharing' => true";
+
+$stmt = $app->getPDO()->query('SELECT * FROM `sites`');
+$stmt->setFetchMode(PDO::FETCH_OBJ);
+
+while ($site = $stmt->fetch()) {
+  print $site->name;
+
+  $sitePath = $rootPath . $site->name;
+  printMsg(file_exists($sitePath), 'path', 'path');
+
+  $apiFile = $sitePath . '/plugins/advert/api.php';
+  printMsg(file_exists($apiFile), 'api', 'api');
+  if (file_exists($apiFile)) {
+//    printMsg(copy($newApiFile, $apiFile), 'copy', 'copy');
+  }
+
+  $cfgFile = $sitePath . '/config/local.php';
+  printMsg(file_exists($cfgFile), 'config', 'config');
+
+  if (file_exists($apiFile)) {
+//    $cfg = file_get_contents($cfgFile);
+//    $cfgResponse = file_put_contents($cfgFile, preg_replace($cfgPattern, $cfgReplacement, $cfg));
+//    printMsg($cfgResponse !== false, 'replace', 'replace');
+  }
+
+  $applicationFile = $sitePath . '/config/application.php';
+  printMsg(file_exists($applicationFile), 'application', 'application');
+
+  if (file_exists($apiFile)) {
+//    $application = file_get_contents($applicationFile);
+//    $applicationResponse = file_put_contents($applicationFile, preg_replace($cfgPattern, "", $application));
+//    printMsg($applicationResponse !== false, 'replace', 'replace');
+  }
+  print '<br/>';
+}
+
+function printMsg(bool $bool, $msgYes, $msgNo) {
+  print ($bool)
+    ? ' - <span style="color:green;">'.$msgYes.'</span>'
+    : ' - <strong style="color:red;">'.$msgNo.'</strong>';
+}
---
+++ src/install/api.php
@@ -0,0 +1,87 @@
+<?php
+
+class advert_api {
+  /**
+   * Collect Data
+   *
+   * @param [int] $advertId
+   *
+   * @return array
+   */
+  static function collectData($advertId): array {
+    $response = [];
+    $q = db_query("SELECT * FROM `advert` WHERE `advert_id`='$advertId' LIMIT 1");
+    $advert = db_result($q);
+    if (!$advert)
+      return [];
+
+    unset($advert['remote_site'], $advert['remote_id'], $advert['advert_id'], $advert['site_id'], $advert['user_id'], $advert['extra4']);
+    $response['advert'] = $advert;
+    $group = $advert['group'];
+
+    if (in_array($group, ['car', 'job', 'property'])) {
+      $q = db_query("SELECT * FROM `$group` WHERE `advert_id`=$advertId");
+      $response[$group] = db_result($q);
+      unset($response[$group][$group . '_id'], $response[$group]['advert_id']);
+
+      $featureTable = $group . '_features';
+      $response[$featureTable] = [];
+      $q = db_query("SELECT `feature_id` FROM `" . $featureTable . "` WHERE `" . $group . "_id` = $advertId");
+      while ($row = db_result($q))
+        $response[$featureTable][] = $row['feature_id'];
+    }
+
+    return $response;
+  }
+
+  /**
+   * @param int $advertId
+   *
+   * @return bool|string
+   */
+  static function sync(int $advertId): bool {
+    $data = self::collectData($advertId);
+    if (sizeof($data) == 0)
+      return false;
+
+    return self::addJobToQueue($advertId, 'sync', $data);
+  }
+
+
+  /**
+   * @param int $advertId
+   *
+   * @return bool
+   */
+  static function syncDelete(int $advertId): bool {
+    return self::addJobToQueue($advertId, 'delete', null);
+  }
+
+  /**
+   * @param int $advertId
+   * @param string $action
+   * @param array|null $data
+   *
+   * @return bool
+   */
+  private static function addJobToQueue(int $advertId, string $action, array $data = null): bool {
+    if (env('advert-sharing') !== true)
+      return false;
+
+    try {
+      $file = (env('advert-queue-file'))
+        ?: LIBRARY . '../shared-advert-library/import.php';
+
+      if (!file_exists($file))
+        return false;
+
+      $queueService = require_once $file;
+      $queueService->addJob(env('advert-site-id'), $advertId, $action, $data);
+
+      return true;
+    } catch (Exception $e) {
+      return false;
+    }
+  }
+}
+
